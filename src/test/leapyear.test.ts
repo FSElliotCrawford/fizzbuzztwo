c33775d0104277ee7735aed4fe58b5210d282bb2
Elliot Crawford <elliot@focalstrategy.com>
2020-12-10 11:44:21 +0000

    Fix test and add new one

--- src/test/leapyear.test.ts
+++ src/test/leapyear.test.ts
@@ -1,5 +1,11 @@
+import {isLeapYear} from "../main/leapyear";
+
 describe('leap year', () => {
     it('should return true for 1996', () => {
         expect(isLeapYear(1996)).toBe(true);
     });
+
+    it('should return false for 2001', () => {
+        expect(isLeapYear(2001)).toBe(false);
+    });    
 });
