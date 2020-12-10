15841435129f064422a05ebcf7497bb1d860f003
Maria Kozlowska <mery.kozlowska@gmail.com>
2020-12-10 11:01:35 +0000

    refactor tests

--- src/main/fizzbuzz.ts
+++ src/main/fizzbuzz.ts
@@ -1,8 +1,4 @@
 export const fizzBuzz = (num: number): string => {
-    // if(num % 15 === 0) {
-    //     return 'fizzbuzz';
-    // }
-
     let response = '';
     
     if(num % 3 === 0) {
--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -1,27 +1,15 @@
 import { fizzBuzz } from "../main/fizzbuzz";
 
 describe('fizzbuzz tests', () => {
-    it("3 should return fizz", () => {
-        expect(fizzBuzz(3)).toBe("fizz");
+    it.each`
+        value | expected
+        ${3}  | ${'fizz'}
+        ${5}  | ${'buzz'}
+        ${15} | ${'fizzbuzz'}
+        ${6}  | ${'fizz'}
+        ${10} | ${'buzz'}
+        ${1}  | ${'1'}
+    `('$value should return $expected', ({value, expected}) => {
+        expect(fizzBuzz(value)).toBe(expected);
     })
-
-    it("5 should return buzz", () => {
-        expect(fizzBuzz(5)).toBe("buzz");
-    })
-
-    it("15 should return fizzbuzz", () => {
-        expect(fizzBuzz(15)).toBe("fizzbuzz");
-    })
-
-    it('6 should return fizz', () => {
-        expect(fizzBuzz(6)).toBe("fizz");
-    });
-
-    it('10 should return buzz', () => {
-        expect(fizzBuzz(10)).toBe("buzz");
-    });
-
-    it('1 should return "1"', () => {
-        expect(fizzBuzz(1)).toBe("1");
-    });
 })
